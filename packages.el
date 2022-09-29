ca09ed7a65ece188f9f4259fb3e14c808e299319
Tian Xin <tian.xin@fb.com>
2022-09-28 19:49:08 -0700

    packages: remove use-package! usages

--- packages.el
+++ packages.el
@@ -59,31 +59,8 @@
 (unpin! org-roam)
 (package! org-roam-ui)
 
-(use-package! websocket
-    :after org-roam)
-
-(use-package! org-roam-ui
-    :after org-roam ;; or :after org
-;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
-;;         a hookable mode anymore, you're advised to pick something yourself
-;;         if you don't care about startup time, use
-;;  :hook (after-init . org-roam-ui-mode)
-    :config
-    (setq org-roam-ui-sync-theme t
-          org-roam-ui-follow t
-          org-roam-ui-update-on-save t
-          org-roam-ui-open-on-start t))
-
 ;; org-alert
 (package! org-alert)
-(use-package! org-alert
-    :after org-roam ;; or :after org
-    :config
-    (lambda ()
-      (org-alert-enable)
-      ; Enable osx notifier if system time is Darwin (MacOS)
-      (if (eq system-type 'darwin)
-          (setq alert-default-style 'osx-notifier))))
 
 ;; tldr
 (package! tldr)
