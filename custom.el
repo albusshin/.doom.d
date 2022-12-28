ca16ad5ac701d9640c94cbd5844b1f9e9aa4ce3e
Tian Xin <tian.xin@fb.com>
2022-12-28 15:32:02 -0800

    org-roam: move dir to iCloud for Mac OS

--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-roam-directory
+   (if (eq system-type 'darwin) "/Users/txin/Library/Mobile Documents/iCloud~com~logseq~logseq/Documents/roam/"))
  )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
